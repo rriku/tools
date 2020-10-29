14249a46851f17d8a3e22b45d9d52a90fbe841c4
rriku <68044945+rriku@users.noreply.github.com>
2020-10-29 12:16:37 +0900

    u

--- dependent_check_bookmark.js
+++ dependent_check_bookmark.js
@@ -1,7 +1,7 @@
 javascript: mu = "";
 var trgtStr = window.getSelection().toString();
 trgtStr = trgtStr.replace(/\r?\n/g, "__br__");
-trgtStr = trgtStr.replace(/?/g, "__?__");
-trgtStr = trgtStr.replace(/=/g, "__=__");
+trgtStr = trgtStr.replace(/\?/g, "__HATENA__");
+trgtStr = trgtStr.replace(/=/g, "__EQUAL__");
 var toolUrl = "https://rriku.github.io/tools/dependent_check.html";
 window.open( toolUrl + "?trgtStr=" + trgtStr);
